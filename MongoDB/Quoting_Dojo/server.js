1771eb9fa421bcf979533cc3b388f137ac55c396
VT-Zhang <jianz@vt.edu>
2017-04-09 18:28:04 -0400

    Made changes on Quoting_Dojo assignment.

--- MongoDB/Quoting_Dojo/server.js
+++ MongoDB/Quoting_Dojo/server.js
@@ -5,19 +5,14 @@ var path = require("path");
 var app = express();
 // require bodyParser since we need to handle post data for adding a user
 var bodyParser = require("body-parser");
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/quotes');
-var QuoteSchema = new mongoose.Schema({
-  name: String,
-  quote: String
-})
-var Quote = mongoose.model('Quote', QuoteSchema);
 app.use(bodyParser.urlencoded({ extended: true }));
 // static content
 app.use(express.static(path.join(__dirname, "./static")));
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
+require("./server/config/mongoose.js");
 // root route to render the index.ejs view
 var routes_setter = require("./server/config/routes.js")
 routes_setter(app);
