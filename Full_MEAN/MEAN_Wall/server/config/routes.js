cc754fa9eb7f7e8c208793428e7aaa7f9ac4a0d9
VT-Zhang <jianz@vt.edu>
2017-04-15 18:12:02 -0400

    Started MEAN_Wall assignment.

---
+++ Full_MEAN/MEAN_Wall/server/config/routes.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+var users = require("./../controllers/users.js");
+
+module.exports = function(app){
+    app.get("/", fucntion(req, res){
+        
+    })
+}
---
+++ Full_MEAN/MEAN_Wall/server/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username = {type: String, required: "Username must be provided", minlength: 2},
+    messages = [{type: Schema.Types.ObjectId, ref:"Message"}],
+    comments = [{type: Schema.Types.ObjectId, ref:"Comment"}]
+}, {timestamps: true});
+
+mongoose.model("User", UserSchema);
